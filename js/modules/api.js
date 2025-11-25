5122be161c2b82a68274d8c21dad8fe7ce6991db
Kez 92 <kez92@MacBook-Air-Kez.local>
2025-11-25 13:31:17 +0300

    done

---
+++ js/modules/api.js
@@ -0,0 +1,30 @@
+const BASE_URL = "https://wedev-api.sky.pro/api/v1";
+const PERSONAL_KEY = "mark-zabaliev";
+
+export function getComments() {
+    return fetch(`${BASE_URL}/${PERSONAL_KEY}/comments`, {
+        method: "GET"
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка загрузки комментариев');
+            }
+            return response.json();
+        });
+}
+
+export function postComment({ name, text }) {
+    return fetch(`${BASE_URL}/${PERSONAL_KEY}/comments`, {
+        method: "POST",
+        body: JSON.stringify({
+            name: name,
+            text: text
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка отправки комментария');
+            }
+            return response.json();
+        });
+}
